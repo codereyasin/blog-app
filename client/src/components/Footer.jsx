1362a134ee11fdf63886125661d420ef6402a999
codereyasin <eyasindevofficial@gmail.com>
2022-11-25 07:06:12 +0600

    blog

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+     <h1>Eyasin Blog</h1>
+      <span>
+        Made with ♥️ and <b>Eyasin Arafat</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
